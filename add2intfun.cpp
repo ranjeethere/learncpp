56f16b020222fa5154a30243424da1b20aff9f27
Ranjeet Yadav <147843523+ranjeethere@users.noreply.github.com>
2023-11-28 01:46:35 +0530

    Add files via upload

---
+++ add2intfun.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+using namespace std ;
+
+int add(int num1,int num2){
+    int sum = num1+num2;
+    return sum;
+}
+
+int main(){
+
+    int a =2;
+    int b = 45;
+    cout<<add(a,b)<<endl;
+    return 0;
+}
