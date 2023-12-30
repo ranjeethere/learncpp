f5b574ec6bbb52ad81899e78ce2b472b68853479
Ranjeet Yadav <147843523+ranjeethere@users.noreply.github.com>
2023-12-30 23:08:10 +0530

    Add files via upload

---
+++ 53.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class A
+{
+    int a ;
+    public:
+     A & setData(int a){
+        this->a = a;
+        return *this;
+
+
+ }
+  void getData(){
+      cout<<"The value of a is :-- "<<a<<endl;
+  }  
+};
+int main ()
+{// this is a keyword which is a pointer which points to the objects which invokes the member function
+    A a;
+    a.setData(4).getData();
+    //a.getData();
+    return 0;
+}
